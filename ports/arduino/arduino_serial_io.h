77cd42f0838be502417c976b4d587be09275e555
nyuuandmee <nyuuandmee@outlook.com>
2026-01-11 13:39:01 -0800

    Add ports/arduino/arduino_serial_io.

---
+++ ports/arduino/arduino_serial_io.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "mc/io.h"
+#include <Arduino.h>
+
+// Macro for defining analog context. Users should always use this.
+#define MC_DEFINE_DEFAULT_ARDUINO_SERIAL_IO(NAME) \
+    static mc_arduino_serial_io_ctx_t NAME = {    \
+        .stream = &Serial,                        \
+        .is_connected = mc_arduino_serial_io_default_is_connected}
+
+// Ctx for arduino serial IO driver
+typedef struct mc_arduino_serial_io_ctx_t
+{
+    void *stream;               // Pointer to the Arduino Stream (Serial)
+    bool (*is_connected)(void); // Callback to check connection status
+} mc_arduino_serial_io_ctx_t;
+
+// Driver to Arduino serial IO
+extern const mc_io_driver_t mc_arduino_serial_io_driver;
+
+// Default is connected function
+inline bool mc_arduino_serial_io_default_is_connected()
+{
+    return (bool)Serial;
+}
